c48a04e063dc89af3b0a2c2b6ca046a70763124a
EXCELINDIA\robin.perera <robin.perera@excelindia.com>
2024-09-12 15:55:24 +0530

    back-end steup

---
+++ back-end/vibeshare/src/main/java/com/vibeshare/VibeshareApplication.java
@@ -0,0 +1,13 @@
+package com.vibeshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeshareApplication.class, args);
+	}
+
+}
